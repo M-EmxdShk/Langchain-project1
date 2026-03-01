2f8c5ea6aeaf68404def8db452287882982db777
Emaduddin <emaduddinfrosty@gmail.com>
2026-03-01 16:31:56 +0400

    Program for business name generator

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ def generate_business_name(expertise: str) -> str:
     """Generate a unique name for a business specializing in {expertise}"""
     llm = ChatGroq(model="openai/gpt-oss-20b", temperature=0.7)
     
-    prompt = f"Generate a single creative and a name for a business specialising in {expertise}. Just the name, nothing else."
+    prompt = f"Generate 5 creative and a name for a business specialising in {expertise}. Just the name, nothing else."
     response = llm.invoke(prompt)
     
     return response.content.strip()
 
 if __name__ == "__main__":
-    business_name = generate_business_name("Marketing")
-    print(f"Generated pet name: {business_name}")
+    business_name = generate_business_name("Business and IT services")
+    print(f"Generated name: {business_name}")
