ac1faa955ed9ccb4ad38a30ea393c0314b7575f2
Emaduddin <emaduddinfrosty@gmail.com>
2026-03-02 17:03:04 +0400

    Connected the streamlit app with Langchain Logic

---
+++ LangchainHelper.py
@@ -0,0 +1,32 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0.3)
+
+def generate_missileinfo(missile):
+    missileOrigin = PromptTemplate(
+        input_variables=['missile'],
+        template="Where is this {missile} from?"
+    )
+
+    missileStrength = PromptTemplate(
+        input_variables=['missile'],
+        template="What is the strength point of this {missile}, And why is it deadly? Only one point."
+    )
+
+    origin_chain = missileOrigin | llm | StrOutputParser()
+    strength_chain = missileStrength | llm | StrOutputParser()
+
+    chain = RunnableParallel({
+        "origin": origin_chain,
+        "strength": strength_chain
+    })
+
+    response = chain.invoke({"missile": missile})
+
+    return response
---
+++ StreamlitLib.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import LangchainHelper
+
+st.title("Missile and its Origin and Strength")
+missile= st.sidebar.selectbox("Pick a Missile", ("Shaheen 1-A", "RS-28 Sarmat", "Hwasong-17", "M51", "Trident II D5"))
+
+if missile:
+    response = LangchainHelper.generate_missileinfo(missile)
+
+    st.subheader("Country of Origin")
+    st.write(response["origin"])
+
+    st.subheader("Strength")
+    st.write(response["strength"])
